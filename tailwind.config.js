74600a1d6e3e565537ddb8d38e5170bd05e2b1dc
Matthew Fogarty <mattempyre@gmail.com>
2023-09-08 09:59:45 +0200

    Create Vite Project and add Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3490dc',
+        secondary: '#ffed4a',
+        danger: '#e3342f',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
